25f69135a17d1c21a8eee9b4bf7357b9ae0b34e3
Luan Oliveira <luanoliveira@Luans-MacBook-Pro.local>
2024-08-12 00:48:30 -0300

    feat: add header components and banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,32 @@
 import Header from "@/components/header"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { SearchIcon } from "lucide-react"
+import Image from "next/image"
 
 export default function Home() {
   return (
     <>
       <div>
         <Header />
+        <div className="p-5">
+          <h2 className="text-xl font-bold">Olá, Luan!</h2>
+          <p>Segunda-feira, 12 de agosto.</p>
+          <div className="mt-6 flex items-center gap-2">
+            <Input placeholder="Buscar" />
+            <Button>
+              <SearchIcon />
+            </Button>
+          </div>
+          <div className="relative mt-6 h-[150px] w-full">
+            <Image
+              src="banner-01.svg"
+              alt="Services"
+              fill
+              className="rounded-xl object-cover"
+            />
+          </div>
+        </div>
       </div>
     </>
   )
